c2feccfa24a3c50b9e669d2e0c33c610a30680a6
mofrey-max <manugodfrey8@gmail.com>
2020-02-09 01:05:25 +0100

    update

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -47,6 +47,42 @@ export class DataService {
         title:  'The God of Abraham praise!',
         url:  '/hymn0001'
       },
+      {
+        id: '3',
+        index: '0003',
+        title:  'The God of Abraham praise!',
+        url:  '/hymn0001'
+      },
+      {
+        id: '4',
+        index: '0004',
+        title:  'The God of Abraham praise!',
+        url:  '/hymn0001'
+      },
+      {
+        id: '5',
+        index: '0005',
+        title:  'The God of Abraham praise!',
+        url:  '/hymn0001'
+      },
+      {
+        id: '3',
+        index: '0003',
+        title:  'The God of Abraham praise!',
+        url:  '/hymn0001'
+      },
+      {
+        id: '4',
+        index: '0004',
+        title:  'The God of Abraham praise!',
+        url:  '/hymn0001'
+      },
+      {
+        id: '5',
+        index: '0005',
+        title:  'The God of Abraham praise!',
+        url:  '/hymn0001'
+      },
 
 
     ];
